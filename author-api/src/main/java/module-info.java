49b31c1af4797ee1b2616e004671267fb2db77d8
Ritotwo <eeren881@gmail.com>
2022-03-23 02:15:22 +0300

    + module info added.

--- author-api/src/main/java/module-info.java
+++ author-api/src/main/java/module-info.java
@@ -1,3 +1,14 @@
 module online.library.author {
+    requires spring.boot;
+    requires spring.boot.autoconfigure;
+    requires spring.data.commons;
+    requires spring.data.jpa;
+    requires spring.context;
+    requires spring.web;
+    requires com.fasterxml.jackson.annotation;
+    requires lombok;
+    requires java.persistence;
     exports online.library.author;
+    exports online.library.author.api;
+    exports online.library.author.model;
 }
