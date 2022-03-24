32c24735443d9e1e17397876c2530806284e8e71
Ritotwo <eeren881@gmail.com>
2022-03-25 00:59:05 +0300

    + discovery server added.

--- author-api/src/main/java/online/library/author/AuthorApiApplication.java
+++ author-api/src/main/java/online/library/author/AuthorApiApplication.java
@@ -2,8 +2,10 @@ package online.library.author;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class AuthorApiApplication {
 
     public static void main(String[] args) {
--- book-api/src/main/java/online/library/book/service/AuthorMicroservice.java
+++ book-api/src/main/java/online/library/book/service/AuthorMicroservice.java
@@ -3,5 +3,5 @@ package online.library.book.service;
 import online.library.author.api.AuthorApi;
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(value = "localhost:9002", url = "http://localhost:9002/author")
+@FeignClient(value = "author-api", path = "/author")
 public interface AuthorMicroservice extends AuthorApi {}
